f7a4274707e6a094a743c49ac2c4d760bb1d73ad
Gabi <iliescugabriel33@gmail.com>
2023-09-28 17:25:59 +0300

    Finished the 5 round loop

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
+const choices = ['Rock', 'Paper', 'Scissors'];
 
-function getComputerChoice(words) {
-    var words = ['Rock', 'Paper', 'Scissor']
-    return words = words[Math.floor(Math.random()*words.length)];
+
+function getComputerChoice(){
+    return choices[Math.floor(Math.random()*choices.length)]
 }
-var words = ['Rock', 'Paper', 'Scissor']
-console.log(getComputerChoice(words))
+function checkWinner(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "Rock" && computerSelection == "Scissors") || 
+        (playerSelection == "Paper" && computerSelection == "Rock") || 
+        (playerSelection == "Scissors" && computerSelection == "Paper")
+        ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It'a tie!"
+    }
+    else if(result == "Player"){
+        return `You win! ${playerSelection} beats ${computerSelection}.`; 
+    }
+    else {
+        return `You lost! ${computerSelection} beats ${playerSelection}.`;
+    }
+}
+
+function game(){
+    for (let i =0; i<5; i++){
+        const playerSelection = "Rock";
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+game();
